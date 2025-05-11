34e9b073aea1b0050dfee11b49eb81558aeaf00a
FabianaR <rossifabiana930@gmail.com>
2025-05-11 19:12:42 -0300

    Leve avance.

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-let opcion
+let opcion;
+const tasadecambio = 1300;
+
+while (opcion !== "2") {
+    opcion = prompt(`1) Divisa
+    2) Salir
+    (ARS -> USD)`);
+}
 
 
-prompt(`1) Divisa             (ARS -> USD)
-    2) Salir`);
+if (opcion = "2") {
+    alert("Programa finalizado.");
+}
 
-    
+function convertCurrency(pesos) {
+    return pesos / tasadecambio;
+}
 
+const USD = convertCurrency(pesos);
+
+if (opcion = "1") {
+    pesos = prompt("Ingrese la cantidad de ARS a convertir.");
+    alert(`Su resultado es de ${USD}`);
+}
