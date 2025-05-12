a651c818f0da7bc3e42c37ea2cbac94d98a9d7e9
FabianaR <rossifabiana930@gmail.com>
2025-05-11 21:31:35 -0300

    .

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-let opcion
+const tasadecambio = 1300;
+const history = [];
 
+function convertCurrency(pesos) {
+    return pesos / tasadecambio;
+}
 
-prompt(`1) Divisa             (ARS -> USD)
-    2) Salir`);
+function menu() {
+    while (true) {
+        let opcion = prompt(`1) Divisa\n2) Salir\n(ARS -> USD)`).trim();
+        if (opcion === "1") {
+            let pesos = parseFloat(prompt("Ingrese la cantidad de ARS a convertir."));
+            const USD = convertCurrency(pesos);
+            alert(`Su resultado es de ${USD.toFixed(2)} USD.`);
+            let conversion = {
+                tipo: 'Divisa',
+                entrada: `${pesos} ARS`,
+                salida: `${USD.toFixed(2)} USD`,
+                fecha: new Date().toLocaleDateString()
+            };
+            history.push(conversion);
+        }
+        else if (opcion === "2") {
+            alert("Programa finalizado.");
+            console.log("Historial de conversiones:", history);
+            return;
+        }
+        else {
+            alert("Opción no válida.");
+            return;
+        }
+    }
+}
 
-    
-
+menu();
